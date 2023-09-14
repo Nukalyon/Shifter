b928fe3cd62466e96b4b74c5408d92ce3e9f9968
Nukalyon <frutos.jeremy@yahoo.fr>
2023-09-14 10:35:59 -0400

    Add Asset

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,29 +3,48 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    SpriteRenderer sr;
     Vector2 speed = new Vector2(50, 50);
     public string axisX;
     public string axisY;
+    bool facingRight = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         //Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         float dirX = Input.GetAxisRaw(axisX);
         float dirY = Input.GetAxisRaw(axisY);
         GetKeyCode();
-
         Vector3 move = new Vector3(dirX * speed.x, dirY * speed.y, 0);
 
         move *= Time.deltaTime;
 
         transform.Translate(move);
+
+        if(dirX < 0)
+        {
+            Flip(false);
+        }
+        if(dirX > 0)
+        {
+            Flip(true);
+        }
+    }
+
+    void Flip(bool val)
+    {
+        sr.flipX = val;
+
+        facingRight = !facingRight;
     }
 
     private void GetKeyCode()
